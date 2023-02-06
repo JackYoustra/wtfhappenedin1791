aa672bb5a88a2992ff35f7346a9b2ead4844b1a1
Jack Youstra <jack@youstra.com>
2023-02-05 16:23:46 -0800

    Other page looking ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,31 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div className="container">
+      <main className="main">
+        <h1 className="title">
+          WTF Happened in 1791?
+        </h1>
+        {children}
+              </main>
+
+      <footer className="footer">
+        <div>
+          {"\"Inspired\" by:"} <a href="https://wtfhappenedin1971.com/">WTF Happened in 1971?</a>
+        </div>
+
+        <div className="footerContent">
+          <a href="https://jackyoustra.com/">Newsletter</a>
+          <a href="https://www.nber.org/subscribe">Get WTF 1791 Merch</a>
+          <hr />
+          <a href="/donations">Support Our Research</a>
+          <a href="https://www.igmchicago.org/igm-economic-experts-panel/">Discussions</a>
+        </div>
+        
+      </footer>
+    </div>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-import Script from 'next/script'
 import styles from './page.module.css'
 
 const fredStyle = {
@@ -10,11 +8,7 @@ const fredStyle = {
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          WTF Happened in 1791?
-        </h1>
+    <div className="main">
         <div className="embed-container">
         <br />
         <iframe src="https://fred.stlouisfed.org/graph/graph-landing.php?g=X4TX&width=670&height=475" scrolling="no" frameBorder={0} style={fredStyle} loading="lazy"></iframe>
@@ -44,23 +38,6 @@ export default function Home() {
         <br />
         {`For a \"good money\" try `} <a href="https://www.getmonero.org/library/Zero-to-Monero-1-0-0.pdf">something actually anonymous</a>.
         </div>
-
-      </main>
-
-      <footer className={styles.footer}>
-        <div>
-          {"\"Inspired\" by:"} <a href="https://wtfhappenedin1971.com/">WTF Happened in 1971?</a>
-        </div>
-
-        <div className={styles.footerContent}>
-          <a href="https://jackyoustra.com/">Newsletter</a>
-          <a href="https://www.nber.org/subscribe">Get WTF 1791 Merch</a>
-          <hr />
-          <a href="/donations">Support Our Research</a>
-          <a href="https://www.igmchicago.org/igm-economic-experts-panel/">Discussions</a>
-        </div>
-        
-      </footer>
-    </div>
+      </div>
   )
 }
